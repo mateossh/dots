08903acbc5e148c2dd76d2a66c1b5fc3112b1361
Mateusz Ż <zochowski.mateusz@outlook.com>
2022-03-12 14:52:43 +0100

    nvim: use packer instead of paq

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,37 +1,43 @@
-vim.cmd 'packadd paq-nvim'         -- Load package
-local paq = require'paq-nvim'.paq  -- Import module and bind `paq` function
-paq { 'savq/paq-nvim', opt=true }     -- Let Paq manage itself
+vim.cmd [[packadd packer.nvim]]
 
--- Add your packages
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use { 'wbthomason/packer.nvim' }
 
-paq 'tpope/vim-commentary'
-paq 'scrooloose/nerdtree'
+  use { 'tpope/vim-commentary' }
+  use { 'scrooloose/nerdtree' }
 
-paq 'neovim/nvim-lspconfig'
-paq 'hrsh7th/nvim-cmp'
-paq 'hrsh7th/cmp-buffer'
-paq 'hrsh7th/cmp-path'
-paq 'hrsh7th/cmp-nvim-lsp'
+  use { 'neovim/nvim-lspconfig' }
+  use { 'hrsh7th/nvim-cmp' }
+  use { 'hrsh7th/cmp-buffer' }
+  use { 'hrsh7th/cmp-path' }
+  use { 'hrsh7th/cmp-nvim-lsp' }
 
-paq 'nvim-lua/popup.nvim'
-paq 'nvim-lua/plenary.nvim'
-paq 'nvim-telescope/telescope.nvim'
+  use { 'nvim-lua/popup.nvim' }
+  use { 'nvim-lua/plenary.nvim' }
+  use { 'nvim-telescope/telescope.nvim' }
 
-paq { 'nvim-treesitter/nvim-treesitter', run = TSUpdate }
+  use { 'nvim-treesitter/nvim-treesitter', run = TSUpdate }
 
-paq 'NTBBloodbath/rest.nvim'
-paq 'lewis6991/gitsigns.nvim'
-paq 'ahmedkhalf/project.nvim'
-paq 'numToStr/comment.nvim'
-paq 'TimUntersberger/neogit'
+  use { 'NTBBloodbath/rest.nvim' }
+  use { 'lewis6991/gitsigns.nvim' }
+  use { 'ahmedkhalf/project.nvim' }
+  use { 'numToStr/comment.nvim' }
+  use { 'TimUntersberger/neogit' }
+
+  use { 'nvim-orgmode/orgmode' }
 
 
-paq 'ghifarit53/daycula-vim'
-paq 'joshdick/onedark.vim'
-paq 'tek256/simple-dark'
-paq 'rakr/vim-two-firewatch'
-paq 'junegunn/seoul256.vim'
-paq 'sts10/vim-pink-moon'
-paq 'adigitoleo/vim-mellow'
-paq 'savq/melange'
-paq 'Mangeshrex/uwu.vim'
+  use { 'ghifarit53/daycula-vim' }
+  use { 'joshdick/onedark.vim' }
+  use { 'tek256/simple-dark' }
+  use { 'rakr/vim-two-firewatch' }
+  use { 'junegunn/seoul256.vim' }
+  use { 'sts10/vim-pink-moon' }
+  use { 'adigitoleo/vim-mellow' }
+  use { 'savq/melange' }
+  use { 'Mangeshrex/uwu.vim' }
+  use { 'owickstrom/vim-colors-paramount' }
+  use { 'relastle/bluewery.vim' }
+
+end)
