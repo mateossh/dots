2aa00cd067171487f1fdc8a5f285cf23e6110605
Mateusz Ż <zochowski.mateusz@outlook.com>
2023-03-30 21:36:58 +0200

    nvim: fix cmp related warning when starting

--- nvim/lua/mateossh/init.lua
+++ nvim/lua/mateossh/init.lua
@@ -63,7 +63,7 @@ vim.api.nvim_set_keymap("v", "<leader>rv", ":lua require('refactoring').debug.pr
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 
 
